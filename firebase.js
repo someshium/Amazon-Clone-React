285ca77449c495f95410ff33e1562a3cdd92b59f
someshium <someshjdh2018@gmail.com>
2021-05-22 19:04:19 +0530

    day3 auth,redux and chekoutpage

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBB4UnCSWn-iHV2yOrjyTFfReEtzRDWaFU",
+    authDomain: "clone-react-ec0c2.firebaseapp.com",
+    projectId: "clone-react-ec0c2",
+    storageBucket: "clone-react-ec0c2.appspot.com",
+    messagingSenderId: "541830999924",
+    appId: "1:541830999924:web:49189eb648a09a2f6d4621",
+    measurementId: "G-4XDHMWC9BB"
+  };
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+
+
+//global store setup
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
